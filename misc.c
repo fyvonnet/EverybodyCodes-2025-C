92bfa12310004583779ea96401a60b60b07488cd
Franck YVONNET <franck@yvonnet.fr>
2025-11-12 09:55:24 +0100

    added binsearch function

--- misc.c
+++ misc.c
@@ -17,3 +17,23 @@ int modulo(int a, int b)
 {
     return ((a % b) + b) % b;
 }
+
+void *binsearch(void *base, int nmemb, int size, int (*compar)(const void *, const void *), void *target)
+{
+    int l = 0;
+    int r = nmemb - 1;
+
+    while (l <= r) {
+        int m = l + ((r - l) >> 1);
+        void *ptr = base + (m * size);
+        int diff = compar(ptr, target);
+        if (diff < 0)
+            l = m + 1;
+        else if (diff > 0)
+            r = m - 1;
+        else
+            return ptr;
+    }
+    return NULL;
+}
+
--- misc.h
+++ misc.h
@@ -2,3 +2,5 @@
 
 char *myfgets(char * restrict s, int size, FILE *stream);
 int modulo(int a, int b);
+void *binsearch(void *base, int nmemb, int size, int (*compar)(const void *, const void *), void *target);
+
