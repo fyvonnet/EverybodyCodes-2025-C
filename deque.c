e4153133b360eee03c15a42d88c4d5c3d61d2e32
Franck YVONNET <franck@yvonnet.fr>
2025-11-13 06:59:36 +0100

    first commit

---
+++ deque.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "deque.h"
+
+
+
+extern deque *deque_create()
+{
+    deque *d = (deque *)malloc(sizeof(deque));
+    d->head = d->tail = NULL;
+    return d;
+}
+
+
+
+extern void* deque_pop_head(deque *d)
+{
+    if (d->head == NULL)
+        return NULL;
+
+    deque_node *n = d->head;
+    void *v = d->head->data;
+    d->head = d->head->next;
+
+    if (d->head == NULL)
+        d->tail = NULL;
+
+    free(n);
+    return v;
+}
+
+
+
+extern void deque_push_tail(deque *d, void *v)
+{
+    deque_node *n = (deque_node *)malloc(sizeof(deque_node));
+    n->data = v;
+    n->next = NULL;
+
+    if (d->head == NULL)
+        d->head = d->tail = n;
+    else {
+        deque_node *p = d->tail;
+        d->tail = n;
+        p->next = n;
+    }
+}
+
---
+++ deque.h
@@ -0,0 +1,17 @@
+#pragma once
+
+struct deque_node {
+    void *data;
+    struct deque_node *next;
+};
+typedef struct deque_node deque_node;
+
+struct deque {
+    struct deque_node *head, *tail;
+};
+typedef struct deque deque;
+
+deque *deque_create();
+void* deque_pop_head(deque *d);
+void deque_push_tail(deque *d, void *v);
+
